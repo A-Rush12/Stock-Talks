d44aec9ee1c8f25eeeeee2aa19fe993425699131
Andrew <59183410+A-Rush12@users.noreply.github.com>
2020-06-13 08:58:37 -0400

    Update Stock_Talks.py

--- Stock_Talks.py
+++ Stock_Talks.py
@@ -16,8 +16,8 @@ data = sheet.get_all_records()  # Get a list of all records
 
 def find_ticker(stock_name):
     stock_name = stock_name.replace(" ","+")
-    url = "https://www.google.com/search?q=cheese+stock"
-    url = url.replace("cheese",stock_name)
+    url = "https://www.google.com/search?q=temp+stock"
+    url = url.replace("temp",stock_name)
     google_request = requests.get(url)
     tree = html.fromstring(google_request.content)
     try:
